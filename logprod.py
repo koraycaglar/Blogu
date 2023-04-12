b416304b5b72133931c640e7b97e9b1da5eee998
Koray Çağlar <koraycaglar@pm.me>
2023-04-12 16:26:51 +0300

    Add files via upload

---
+++ logprod.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template
+import logging
+
+app = Flask(__name__)
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
+
+@app.route('/')
+def hello():
+    app.logger.info('Home page viewed')
+    return render_template('home.html')
+
+@app.route('/post', methods=['POST'])
+def create_post():
+    # Code to create a new blog post
+    app.logger.info('New blog post created')
+    return 'New blog post created'
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    # Code to handle user login
+    app.logger.info('User logged in')
+    return 'User logged in'
+
+@app.route('/comment', methods=['POST'])
+def add_comment():
+    # Code to handle user comments
+    app.logger.info('New user comment added')
+    return 'New user comment added'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
